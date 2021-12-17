65657eeb80ff2b70a372e78f5099cea5cd2503ae
graybaelBen <91919760+graybaelBen@users.noreply.github.com>
2021-12-16 19:15:10 -0800

    added comment

--- flann.py
+++ flann.py
@@ -15,6 +15,7 @@ sift = cv2.SIFT_create()
 kp1, des1 = sift.detectAndCompute(img1,None)
 kp2, des2 = sift.detectAndCompute(img2,None)
 
+# everything above here is used for testing the flann implementation below
 # FLANN parameters
 FLANN_INDEX_KDTREE = 0
 index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
