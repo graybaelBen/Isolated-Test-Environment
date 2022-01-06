cb9bf7a10e8d376fc8f5d1c6665c535cc1fe61e7
jc884 <93562473+jc884@users.noreply.github.com>
2022-01-06 13:20:24 -0800

    Fixed masks

--- SIFT_detector_module.py
+++ SIFT_detector_module.py
@@ -5,11 +5,13 @@ class SIFT_detector:
 
     def SIFT_detect():
         filename = '02__Station32__Camera1__2012-8-31__5-17-22(1).JPG'
-        #mask = '02__Station32__Camera1__2012-8-31__5-17-22(1).BMP'
+        maskfile = '02__Station32__Camera1__2012-8-31__5-17-22(1).BMP'
+        mask = cv2.imread(maskfile)
         img = cv2.imread(filename)
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        grayImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        grayMask = cv2.cvtColor(mask,cv2.COLOR_BGR2GRAY)
         sift = cv2.SIFT_create()
-        kp = sift.detect(gray)
+        kp = sift.detect(grayImg, grayMask)
         """ img = cv.drawKeypoints(gray,kp,img)
         cv.imwrite('sift_keypoints.jpg',img) """
-        return kp, gray
+        return kp, grayImg
