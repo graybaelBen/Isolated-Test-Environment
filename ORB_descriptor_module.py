45f79031019eb012f034bbd898cb83dfc7e0bbd8
matthaeus-m <93562430+matthaeus-m@users.noreply.github.com>
2022-03-30 22:19:01 -0700

    ORB fix

--- ORB_descriptor_module.py
+++ ORB_descriptor_module.py
@@ -4,7 +4,7 @@ from ORB_detector_module import ORB_detector
 
 class ORB_descriptor:
     def descript(gray, kp):
-        orb = cv2.ORB_create(nfeatures = 750,WTA_K = 3, edgeThreshold = 31, patchSize = 31, fastThreshold=40)
+        orb = cv2.ORB_create()
         kp, des = orb.compute(gray,kp)
         print('ORB: kp=%d, descriptors=%s' % (len(kp), des.shape))
         #print(des.shape)
--- ORB_detector_module.py
+++ ORB_detector_module.py
@@ -2,14 +2,14 @@ import numpy as np
 import cv2 
 
 class ORB_detector:
-    def ORB_detect(img_filename, mask_filename):
-        img = cv2.imread(img_filename)
-        gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        mask = cv2.imread(mask_filename)
-        gray_mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    def detect(img, mask):
+        # img = cv2.imread(img_filename)
+        # gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        # mask = cv2.imread(mask_filename)
+        # gray_mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
 
-        orb = cv2.ORB_create(nfeatures = 750 ,WTA_K = 3, edgeThreshold = 31, patchSize = 31, fastThreshold = 40)
-        kp = orb.detect(gray_img, gray_mask)
+        orb = cv2.ORB_create()
+        kp = orb.detect(img, mask)
         #print(len(kp))
-        return kp, gray_img
+        return kp
         
