bf47ab6002dc49c343827155926d183d7f29deef
matthaeus-m <93562430+matthaeus-m@users.noreply.github.com>
2022-04-21 15:19:02 -0700

    hess_aff matching sandbox

--- hessian_affine.py
+++ hessian_affine.py
@@ -1,25 +1,61 @@
-
 import pyhesaff
 from pyhesaff._pyhesaff import grab_test_imgpath
 from pyhesaff._pyhesaff import argparse_hesaff_params
-import ubelt as ub
+import os
 import cv2
+import ubelt as ub
+import numpy
+import rhino3dm
+# from cv2 import Point2f
 
+# img_fpath = grab_test_imgpath(ub.argval('--fname', default='astro.png'))
+current_dir = os.path.join('Demo','D1.1')
+imgdir = os.path.join(current_dir,'images')
+img_fpath = os.path.join(imgdir, '02__Station13__Camera1__2012-9-13__2-21-36(2).JPG')
+img_fpath2 = os.path.join(imgdir, '02__Station32__Camera1__2012-7-14__4-48-10(7).JPG')
+print(img_fpath)
 
-#img_fpath = grab_test_imgpath(ub.argval('--fname', default='astro.png'))
-img_fpath = 'Batch1/pristine2send/processed/02__Station13__Camera1__2012-9-13__2-21-40(9).JPG'
 kwargs = argparse_hesaff_params()
 print('kwargs = %r' % (kwargs,))
 
 (kpts, vecs) = pyhesaff.detect_feats(img_fpath, **kwargs)
+(kpts2, vecs2) = pyhesaff.detect_feats(img_fpath2, **kwargs)
 imgBGR = cv2.imread(img_fpath)
 
-print(kpts)
-print("desc", vecs)
-#cv2.imshow("KeyPoints",imgBGR)
-#cv2.waitKey(0)
+# vecs[0], vecs[1]
+# print(vecs[0])
+# print(vecs[0].size)
+
+# vecs2 = vecs
+
+FLANN_INDEX_KDTREE = 0
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks=50)   # or pass empty dictionary
+flann = cv2.FlannBasedMatcher(index_params,search_params)
+matches = flann.knnMatch(vecs,vecs2,k=2)
+m, n = matches
+print(m.distance)
+print(n.distance)
 
 
+
+
+
+# test = rhino3dm.Point2f(0.1,0.2)
+
+# print(type(test))
+# # test = cv.Point2f(0.1,0.2)
+# # cv2.Point2f = test
+
+# # new = Point2f(kpts[0][0], kpts[0][1])
+# # asPoint2f(kpts)
+# # for pair in kpts:
+# #     keypoint
+# new = cv2.KeyPoint_convert(test)
+# print(type(cv2.Point2f))
+# print("desc", vecs)
+# #cv2.imshow("KeyPoints",imgBGR)
+# #cv2.waitKey(0)
 
 """ https://stackoverflow.com/questions/67762285/drawing-sift-keypoints
 Draw keypoints as crosses, and return the new image with the crosses. """
@@ -27,35 +63,32 @@ img_kp = imgBGR.copy()  # Create a copy of img
 
 # Iterate over all keypoints and draw a cross on evey point.
 for kp in kpts:
-    x = kp[0] # Each keypoint as an x, y tuple  https://stackoverflow.com/questions/35884409/how-to-extract-x-y-coordinates-from-opencv-cv2-keypoint-object
-    y = kp[1]
-    x = round(int(x))  # Round an cast to int
-    y = round(int(y))
+    # x, y = kp.pt
+    x = kp[0]
+    y = kp[1]  # Each keypoint as an x, y tuple  https://stackoverflow.com/questions/35884409/how-to-extract-x-y-coordinates-from-opencv-cv2-keypoint-object
+    x = int(round(x))  # Round an cast to int
+    y = int(round(y))
 
     # Draw a cross with (x, y) center
-    cv2.drawMarker(img_kp, (x, y), color = (255,0,0), markerType=cv2.MARKER_CROSS, markerSize=5, thickness=1, line_type=cv2.LINE_8)
+    cv2.drawMarker(img_kp, (x, y), color = (250,0,0), markerType=1, markerSize=5, thickness=1, line_type=cv2.LINE_8)
 #Demo code
 #img_kp = process.draw_cross_keypoints(image, kp, color=(120,157,187))
-cv2.imwrite("/mnt/c/GitHub/Isolated-Test-Environment/test.png",img_kp)
-print("done")
+cv2.imwrite("KeyPoints.jpg",img_kp)
+cv2.waitKey(0)
 
 #return img_kp  # Return the image with the drawn crosses.
 
+# if ub.argflag('--show'):
+#     # Show keypoints
+#     imgBGR = cv2.imread(img_fpath)
+#     default_showkw = dict(ori=False, ell=True, ell_linewidth=2,
+#                             ell_alpha=.4, ell_color='distinct')
+#     print('default_showkw = %r' % (default_showkw,))
+#     #import utool as ut
+#     #showkw = ut.argparse_dict(default_showkw)
+#     #import plottool_ibeis as pt
+#     #pt.interact_keypoints.ishow_keypoints(imgBGR, kpts, vecs, **showkw)
+#     #pt.show_if_requested()
 
-
-"""
-if ub.argflag('--show'):
-    # Show keypoints
-    imgBGR = cv2.imread(img_fpath)
-    default_showkw = dict(ori=False, ell=True, ell_linewidth=2,
-                            ell_alpha=.4, ell_color='distinct')
-    print('default_showkw = %r' % (default_showkw,))
-    #import utool as ut
-    #showkw = ut.argparse_dict(default_showkw)
-    #import plottool_ibeis as pt
-    #pt.interact_keypoints.ishow_keypoints(imgBGR, kpts, vecs, **showkw)
-    #pt.show_if_requested()
-
-    cv2.imshow("Processed Image",imgBGR)
-    cv2.waitKey(0)
-"""
+#     cv2.imshow("Processed Image",imgBGR)
+#     cv2.waitKey(0)
